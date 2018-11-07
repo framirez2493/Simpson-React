918e173be955c658dbf820a3abd851232bedab19
fernando_R <fer0624@gmail.com>
2018-11-06 20:24:35 -0800

    Updates

---
+++ precache-manifest.ea87a1ccd352f1cfe71bef65dc35a582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a74b5a2b872be53716f",
+    "url": "/Simpson-React/static/js/runtime~main.7a74b5a2.js"
+  },
+  {
+    "revision": "7d2d289e8e206ba1fa36",
+    "url": "/Simpson-React/static/js/main.7d2d289e.chunk.js"
+  },
+  {
+    "revision": "91ea072c0dc511c98d6c",
+    "url": "/Simpson-React/static/js/1.91ea072c.chunk.js"
+  },
+  {
+    "revision": "7d2d289e8e206ba1fa36",
+    "url": "/Simpson-React/static/css/main.7a35fbe6.chunk.css"
+  },
+  {
+    "revision": "6957c3d4dc1d716de83f1c29a8e94a7c",
+    "url": "/Simpson-React/index.html"
+  }
+];
